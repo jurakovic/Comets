30c2fab95a1d5de66ef3912f76abadb29b3b8156
jurakovic <17744091+jurakovic@users.noreply.github.com>
2024-05-01 18:59:15 +0200

    publish command (initial); remove publish profile

--- publish.sh
+++ publish.sh
@@ -3,3 +3,5 @@
 latest=$(git show release:version)
 
 echo $latest
+
+dotnet publish -f net8.0-windows -r win-x64 --no-self-contained -p:PublishSingleFile=true -p:AssemblyVersion=1.2.3.4 -p:Version=1.2.3.4
